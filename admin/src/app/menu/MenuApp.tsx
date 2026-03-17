78f0cca5cab068eb680894d6914dbc4c59446d5c
Alan Ferreira da Silva <alan.ferreira1123@gmail.com>
2026-03-16 21:13:21 -0300

    fix(cart): adjust colors to black/white/gray, move upsell to top, reduce header padding

--- admin/src/app/menu/MenuApp.tsx
+++ admin/src/app/menu/MenuApp.tsx
@@ -1555,6 +1555,37 @@ const App = ({ filterCategories = null, searchProductName = null, onBack = null,
                             {/* TAB: CARRINHO */}
                             {cartTab === 'carrinho' && (
                                 <>
+                                    {/* UPSELL SECTION */}
+                                    {Object.keys(cart).length > 0 && products.length > 0 && (
+                                        <div className="cart-upsell-container">
+                                            <h4 className="upsell-title">Que tal adicionar?</h4>
+                                            <div className="upsell-scroll">
+                                                <div className="upsell-marquee">
+                                                    {products
+                                                        .filter(p => !cart[p.id] && !Object.keys(cart).some(k => k.startsWith(p.id)) && p.id !== currentProduct?.id)
+                                                        .slice(0, 5)
+                                                        .map(p => (
+                                                            <div key={p.id} className="upsell-item" onClick={() => {
+                                                                setCart(prev => ({ ...prev, [p.id]: (prev[p.id] || 0) + 1 }));
+                                                            }}>
+                                                                <div className="upsell-img-box">
+                                                                    <img src={p.imageUrl} alt={p.name} />
+                                                                </div>
+                                                                <div className="upsell-info">
+                                                                    <span className="upsell-name">{p.name}</span>
+                                                                    <span className="upsell-price">
+                                                                        {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(p.price)}
+                                                                    </span>
+                                                                </div>
+                                                                <div className="upsell-add-icon">+</div>
+                                                            </div>
+                                                        ))
+                                                    }
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+
                                     {Object.keys(cart).length === 0 ? (
                                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', opacity: 0.5, marginTop: '40px' }}>
                                             <ShoppingCart size={48} color="#888" style={{ marginBottom: '16px' }} />
@@ -1653,36 +1684,7 @@ const App = ({ filterCategories = null, searchProductName = null, onBack = null,
                                         })
                                     )}
 
-                                    {/* UPSELL SECTION */}
-                                    {Object.keys(cart).length > 0 && products.length > 0 && (
-                                        <div className="cart-upsell-container">
-                                            <h4 className="upsell-title">Que tal adicionar?</h4>
-                                            <div className="upsell-scroll">
-                                                <div className="upsell-marquee">
-                                                    {products
-                                                        .filter(p => !cart[p.id] && !Object.keys(cart).some(k => k.startsWith(p.id)) && p.id !== currentProduct?.id)
-                                                        .slice(0, 5)
-                                                        .map(p => (
-                                                            <div key={p.id} className="upsell-item" onClick={() => {
-                                                                setCart(prev => ({ ...prev, [p.id]: (prev[p.id] || 0) + 1 }));
-                                                            }}>
-                                                                <div className="upsell-img-box">
-                                                                    <img src={p.imageUrl} alt={p.name} />
-                                                                </div>
-                                                                <div className="upsell-info">
-                                                                    <span className="upsell-name">{p.name}</span>
-                                                                    <span className="upsell-price">
-                                                                        {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(p.price)}
-                                                                    </span>
-                                                                </div>
-                                                                <div className="upsell-add-icon">+</div>
-                                                            </div>
-                                                        ))
-                                                    }
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )}
+
                                 </>
                             )}
 
